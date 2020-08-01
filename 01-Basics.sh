de39e2cec812428979dcdad47c57c0ad589b79bb
Marcelo <marcelobiagiola01@gmail.com>
2020-08-01 15:33:31 -0400

    conditional statements

--- 01-Basics.sh
+++ 01-Basics.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 # Basics commands
 echo "Enter your name: "
 
---
+++ 03-Conditional-Statements.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+count=11
+
+if [ $count -eq 10 ]
+then
+    echo "the condition is true"
+else
+    echo "the condition is not true"
+fi
+
+# -eq: equal; -ne: not equal; -gt: grater than; -lt: less than
