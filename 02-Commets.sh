33a9063ad4df092ea8e199aeb03c634d32921ca0
Marcelo <marcelobiagiola01@gmail.com>
2020-08-01 09:13:37 -0400

    02-Comments added

---
+++ 02-Commets.sh
@@ -0,0 +1,13 @@
+# This is one line comment
+
+:'
+    this 
+    is 
+    a multiline
+    commet
+'
+# Will print all these lines
+cat << kreativ
+    testing a kreativ tag  
+    second line 
+kreativ
