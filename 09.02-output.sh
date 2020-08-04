fda69a732d3e95e006506f21b1cdadf3f96a08bb
Marcelo <marcelobiagiola01@gmail.com>
2020-08-04 18:28:36 -0400

    outputs without error opt

--- 09.02-output.sh
+++ 09.02-output.sh
@@ -5,3 +5,10 @@
 # and if there's a problem with the command will
 # write a message error in file2.txt
 ls -al 1>file1.txt 2>file2.txt
+
+# this will ignore the error file
+# and if there's an error, will print
+# just in the terminal
+# otherwise will write the file1.txt with
+# out standard output data
+ls -al>file1.txt 
