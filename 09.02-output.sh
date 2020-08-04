3965f8b10f8bc6db5b1c6d31f42948f7626f03f2
David <41311088+biagiola@users.noreply.github.com>
2020-08-04 18:38:38 -0400

    stdout or err msg in one file

--- 09.02-output.sh
+++ 09.02-output.sh
@@ -4,18 +4,18 @@
 # (all the names files, permisions, dates, )
 # and if there's a problem with the command will
 # write a message error in file2.txt
-#ls -al 1>file1.txt 2>file2.txt
+ls -al 1>file1.txt 2>file2.txt
 
 # this will ignore the error file
 # and if there's an error, will print
 # just in the terminal
 # otherwise will write the file1.txt with
 # out standard output data
-#ls -al>file1.txt 
+ls -al>file1.txt 
 
 # we make just one file for our stdout
 # if is ok will write in file1.txt and
 # if there's an error will be re-write
 # the whole file
 ls -al>file1.txt 2>&1
-#ls +al >& file1.txt short version
+ls +al >& file1.txt short version
