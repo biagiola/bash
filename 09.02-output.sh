4008878a7268921313dd7e03322e16066327d6f3
Marcelo <marcelobiagiola01@gmail.com>
2020-08-04 18:24:15 -0400

    outputs comments

--- 09.02-output.sh
+++ 09.02-output.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-ls -al 1>file1.txt 2>file.txt
+# In file1.txt will write the standard output
+# (all the names files, permisions, dates, )
+# and if there's a problem with the command will
+# write a message error in file2.txt
+ls -al 1>file1.txt 2>file2.txt
