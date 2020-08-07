182f3b0daedb99adc813b43f719a14f180229f04
Marcelo <marcelobiagiola01@gmail.com>
2020-08-07 17:07:39 -0400

    functions

--- 15-Functions.sh
+++ 15-Functions.sh
@@ -2,8 +2,10 @@
 
 function funcCheck()
 {
-    value="I love "
-    echo $value $1
+    string1="I love "
+    string2=" and "
+
+    echo $string1 $1 $string2 $2
 }
 
-funcCheck linux 
+funcCheck linux mac
