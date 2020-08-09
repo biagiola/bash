8465ab01fe0b827529abf62148e9ba905ebe68c4
Marcelo <marcelobiagiola01@gmail.com>
2020-08-09 13:44:30 -0400

    read file line by line

---
+++ 17.01-Files.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo "Enter the filename you wanna read"
+read filename
+
+if [[ -f "$filename" ]]
+then
+    # while IFS="" read -r line
+    while IFS= read -r line 
+    do
+        echo "$line"
+    
+    done < $filename # > to write and < to read
+
+else
+    echo "File doesn't exists"
+fi
