e4c095b9c716308a20bc5488c05699fbba3f2319
Marcelo <marcelobiagiola01@gmail.com>
2020-08-09 13:11:28 -0400

    directives

--- 16-Directives.sh
+++ 16-Directives.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
+# pwd show the path
 # mkdir name creates a new folder
 # mkdir -p name recreate the folder even if there's files inside
 # rm -R foldername remove folder
+# move foldername newNameFolder
+# cp copy files and cp -r folders
 
 echo "Enter the directory name to check if it exits"
 read directory
