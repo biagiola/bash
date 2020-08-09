1d5250ba0d1620edc46d1b681aec21b2454ad5f8
Marcelo <marcelobiagiola01@gmail.com>
2020-08-09 13:33:11 -0400

    add and append texts

---
+++ 17-File.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# touch filename creates a file
+# rm filename removes it
+
+echo "Enter filename to check"
+read filename
+
+if [[ -f "$filename" ]]
+then
+    echo "$filename exists. Enter some text there"
+    read fileText
+
+    # append the text on it
+    echo "$fileText" >> $filename
+    # echo "$fileText" > $filename same as above but erase the whole file
+    # first before add the text
+
+
+else
+    echo "$filename doestn exists"
+
+fi
