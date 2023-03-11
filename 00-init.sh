9d515a234cce7d550812e8edc0bfba3d168a8a4c
David <marcelobiagiola01@gmail.com>
2023-03-11 11:36:13 -0300

    specify the lenguage of the file

--- 00-init.sh
+++ 00-init.sh
@@ -4,5 +4,8 @@ touch init.sh
 # give permissions to execute that file
 sudo chmod +x init.sh
 
+# inside the file we must to specify the dialect of
+# our bash file, put it this to the first line: #! /bin/bash
+
 # run the file
 sudo bash init.sh
