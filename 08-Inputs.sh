bbe930bbedd99c86725b3917f03072987bbf6893
David <marcelobiagiola01@gmail.com>
2023-03-11 15:47:26 -0300

    all about inputs in one file

--- 08-Inputs.sh
+++ 08-Inputs.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
+# Arguments passing from the prompt
+echo "Name file:" $0 # prints the file name
+echo "Args:" $1 $2 $3 # prints the arguments that we pass from the terminal
 
-echo $0 # prints the file name
-echo $1 $2 $3 # prints the arguments that we pass from the terminal
+####
+# we can read a file and print it on the prompt
+# to do that, we must to pass name of the file
+# ./08-Intpus.sh readme.txt
+: 'while read var
+do
+  echo "$var"
+done < "${1:-/dev/stdin}" '
